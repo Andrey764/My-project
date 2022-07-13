78f4bcbdd004323bfa66f3b5344e888988e3a376
Andrey764 <74454858+Andrey764@users.noreply.github.com>
2022-07-13 16:17:19 +0300

    Initial commit

---
+++ Assets/Scripts/Geometry.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Geometry
+{
+    static public Vector3 PointFromGrid(Vector2Int gridPoint) => 
+        new Vector3(-3.5f + 1.0f * gridPoint.x, 0, -3.5f + 1.0f * gridPoint.y);
+
+    static public Vector2Int GridPoint(int col, int row) => new Vector2Int(col, row);
+
+    static public Vector2Int GridFromPoint(Vector3 point) => 
+        new Vector2Int(Mathf.FloorToInt(4.0f + point.x), Mathf.FloorToInt(4.0f + point.z));
+}
